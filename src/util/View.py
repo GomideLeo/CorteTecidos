3b4a96b3737e33a51ac1341ef032f1facdcebf03
Leonardo Gomide <leocgomide@gmail.com>
2022-08-10 16:39:27 -0300

    added a visualization module

---
+++ src/util/View.py
@@ -0,0 +1,24 @@
+import numpy as np
+import util.colorGenerator
+import math
+import cv2
+
+view = np.zeros((100, 0, 3), dtype = "uint8")
+colorgen = util.colorGenerator.createColorGenerator()
+
+def destroyAll():
+    cv2.destroyAllWindows()
+
+def reset(width = 500, height = 100):
+    global view
+    global colorgen
+
+    view = np.zeros((height, math.ceil(width), 3), dtype = "uint8")
+    colorgen = util.colorGenerator.createColorGenerator()
+
+def addPoly(poly):
+    cv2.fillPoly(view, poly, next(colorgen))
+
+def show(windowName, delay = 100):
+    cv2.imshow(windowName, view)
+    cv2.waitKey(delay)
