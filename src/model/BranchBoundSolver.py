bd542f229c8743d8c6fe7f3609a43c23fb920118
Leonardo Gomide <leocgomide@gmail.com>
2022-12-03 11:21:13 -0300

    timeouts 😍

--- src/model/BranchBoundSolver.py
+++ src/model/BranchBoundSolver.py
@@ -1,13 +1,20 @@
+import time
 from model.Sheet import Sheet
 
+
 class BranchBoundSolver:
+    TIMEOUT = 60 * 5  # 5 mins
+
     def __init__(self, sheet: Sheet) -> None:
         self.sheet = sheet
         self.bestFit = float('inf')
         self.bestArrangement = []
         self.totalEvaluations = 0
-    
-    def permute(self, toPermute, permuted = []) -> None:
+
+    def permute(self, toPermute, permuted=[]) -> None:
+        if (time.time() - self.startTime > BranchBoundSolver.TIMEOUT):
+            return
+
         if (len(toPermute) == 0):
             self.totalEvaluations += 1
             leafWaste = Sheet(permuted).calculateWaste()
@@ -15,14 +22,15 @@ class BranchBoundSolver:
                 self.bestFit = leafWaste
                 self.bestArrangement = permuted
             return
-        
+
         for i in range(len(toPermute)):
             choice = permuted + [toPermute[i]]
             rest = toPermute[0:i] + toPermute[i + 1:]
             if Sheet(choice).calculateWaste() < self.bestFit:
                 self.totalEvaluations += 1
                 self.permute(rest, choice)
-    
+
     def findSolution(self) -> list:
+        self.startTime = time.time()
         self.permute(self.sheet.pieces[1:])
         return self.bestArrangement
--- src/model/BruteForceSolver.py
+++ src/model/BruteForceSolver.py
@@ -1,15 +1,19 @@
+import time
 from model.Sheet import Sheet
 import util.permutations as permutations
 import cv2
 import util.View as view
 
+
 class BruteForceSolver:
+    TIMEOUT = 60 * 5  # 5 mins
+
     def __init__(self, sheet: Sheet) -> None:
         self.sheet = sheet
         self.bestFit = float('inf')
         self.bestArrangement = []
         self.totalEvaluations = 0
-    
+
     def bruteForce(self, delay=100, displayTest=True, testWinName="testing_views") -> None:
         for arrangement in permutations.permutations(self.sheet.pieces[1:]):
             self.totalEvaluations += 1
@@ -21,9 +25,13 @@ class BruteForceSolver:
             if displayTest:
                 sheetTest.draw(testWinName, delay, 10)
                 cv2.waitKey()
+
+            if (time.time() - self.startTime > BruteForceSolver.TIMEOUT):
+                break
         if displayTest:
             view.destroyWindow(testWinName)
-    
+
     def findSolution(self) -> list:
+        self.startTime = time.time()
         self.bruteForce(displayTest=False)
         return self.bestArrangement
